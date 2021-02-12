8152d78ab8b984dcbbab8c7eba5a58d4b68b24fc
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-12 00:22:35 +0000

    finished

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,22 @@
 const request = require('request');
-const arg = process.argv.slice(2);
-const breed = arg[0];
+// const arg = process.argv.slice(2);
+// const breedName = arg[0];
 
 
-let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breed;
 
-request(url, function(error, response, body) {
-  if (error) {
-    throw error;
-  }
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log('Error: Please input a valid cat breed');
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(url, function(error, response, body) {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      return callback("This is not a breed", null);
+    } else {
+      return callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = {fetchBreedDescription};
